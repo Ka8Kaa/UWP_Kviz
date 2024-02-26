203109ee4c218443386660c99d98c784de236ef1
Ka8Kaa <133215009+Ka8Kaa@users.noreply.github.com>
2024-02-26 23:29:06 +0100

    Add files via upload

--- Kviz.xaml.cs
+++ Kviz.xaml.cs
@@ -18,17 +18,118 @@ namespace UWP_Kviz
     public sealed partial class Kviz : Page
     {
         private List<int> displayedQuestionIds = new List<int>();
-        private int totalNumberOfQuestions = 2;
+        private int totalNumberOfQuestions = 4;
         private Question currentQuestion; // Field to store the current question
+        private bool isFirstPlayerTurn = true;
+        private int firstPlayerScore = 0; // Track first player's score
+        private int secondPlayerScore = 0; // Track second player's score
+        private DispatcherTimer player1Timer;
+        private DispatcherTimer player2Timer;
+        private int player1TimerDuration = 60; // Duration of player 1's timer in seconds
+        private int player2TimerDuration = 60; // Duration of player 2's timer in seconds
+
 
         public Kviz()
         {
             this.InitializeComponent();
             FetchLastTwoRows();
+            InitializeTimers();
+
+        }
+        private void InitializeTimers()
+        {
+            player1Timer = new DispatcherTimer();
+            player1Timer.Interval = TimeSpan.FromSeconds(1);
+            player1Timer.Tick += Player1Timer_Tick;
+
+            player2Timer = new DispatcherTimer();
+            player2Timer.Interval = TimeSpan.FromSeconds(1);
+            player2Timer.Tick += Player2Timer_Tick;
         }
 
+        private void StartPlayer1Timer()
+        {
+            player1Timer.Start();
+        }
+
+        private void StopPlayer1Timer()
+        {
+            player1Timer.Stop();
+        }
+
+        private void ResetPlayer1Timer()
+        {
+            player1Timer.Stop();
+            player1Timer = new DispatcherTimer();
+            player1Timer.Interval = TimeSpan.FromSeconds(1);
+            player1Timer.Tick += Player1Timer_Tick;
+        }
+
+        private void StartPlayer2Timer()
+        {
+            player2Timer.Start();
+        }
+
+        private void StopPlayer2Timer()
+        {
+            player2Timer.Stop();
+        }
+
+        private void ResetPlayer2Timer()
+        {
+            player2Timer.Stop();
+            player2Timer = new DispatcherTimer();
+            player2Timer.Interval = TimeSpan.FromSeconds(1);
+            player2Timer.Tick += Player2Timer_Tick;
+        }
+        private void Player1Timer_Tick(object sender, object e)
+        {
+            player1TimerDuration--;
+            if (player1TimerDuration > 0)
+            {
+                // Update the UI with the remaining time for Player 1
+                PrviIgracVrijeme.Text = player1TimerDuration.ToString() + "s";
+            }
+            else
+            {
+                // Player 1's timer has reached 0
+                StopPlayer1Timer();
+                PogresniOdgovori.Text = "IGRAC DVA JE POBJEDIO!";
+                // Perform any actions you want when the timer reaches 0 for Player 1
+                // For example, end the game or switch to the other player's turn
+            }
+        }
+        private void Player2Timer_Tick(object sender, object e)
+        {
+            player2TimerDuration--;
+            if (player2TimerDuration > 0)
+            {
+                // Update the UI with the remaining time for Player 2
+                DrugiIgracVrijeme.Text = player2TimerDuration.ToString() + "s";
+            }
+            else
+            {
+                // Player 2's timer has reached 0
+                StopPlayer2Timer();
+                PogresniOdgovori.Text = "IGRAC JEDAN JE POBJEDIO!";
+                // Perform any actions you want when the timer reaches 0 for Player 2
+                // For example, end the game or switch to the other player's turn
+            }
+        }
         private void Započni_Click(object sender, RoutedEventArgs e)
         {
+            StopPlayer1Timer();
+            StopPlayer2Timer();
+            ResetPlayer1Timer();
+            ResetPlayer2Timer();
+            if (isFirstPlayerTurn)
+            {
+                StartPlayer1Timer();
+            }
+            else
+            {
+                StartPlayer2Timer();
+            }
             // Fetch a random question
             currentQuestion = GetRandomQuestion();
 
@@ -56,6 +157,7 @@ namespace UWP_Kviz
                     optionRadioButton4.Content = options[3];
 
                     // Make the TextBlock and RadioButtons visible
+                    Provjeri.Visibility = Visibility.Visible;
                     questionTextBlock.Visibility = Visibility.Visible;
                     optionRadioButton1.Visibility = Visibility.Visible;
                     optionRadioButton2.Visibility = Visibility.Visible;
@@ -84,59 +186,64 @@ namespace UWP_Kviz
                 {
                     connection.Open();
 
-                    // SQLite query to get a random question that hasn't been displayed yet
-                    string query = "SELECT * FROM Pitanja WHERE ID NOT IN (" + string.Join(",", displayedQuestionIds) + ") ORDER BY RANDOM() LIMIT 1";
+                    // Fetch all questions
+                    string query = "SELECT * FROM Pitanja";
+                    List<Question> allQuestions = new List<Question>();
 
                     using (SQLiteCommand command = new SQLiteCommand(query, connection))
                     {
                         using (SQLiteDataReader reader = command.ExecuteReader())
                         {
-                            if (reader.HasRows)
+                            while (reader.Read())
                             {
-                                if (reader.Read())
+                                Question question = new Question
                                 {
-                                    // Construct the question object
-                                    Question question = new Question
+                                    Id = reader.GetInt32(0),
+                                    QuestionText = reader.GetString(1),
+                                    CorrectAnswer = reader.GetString(2),
+                                    WrongAnswers = new string[]
                                     {
-                                        Id = reader.GetInt32(0),
-                                        QuestionText = reader.GetString(1),
-                                        CorrectAnswer = reader.GetString(2),
-                                        WrongAnswers = new string[]
-                                        {
-                                            reader.GetString(3),
-                                            reader.GetString(4),
-                                            reader.GetString(5)
-                                        }
-                                    };
-
-                                    // Add the displayed question ID to the list
-                                    displayedQuestionIds.Add(question.Id);
-
-                                    // If all questions have been displayed, reset the list
-                                    if (displayedQuestionIds.Count == totalNumberOfQuestions)
-                                    {
-                                        displayedQuestionIds.Clear();
+                                reader.GetString(3),
+                                reader.GetString(4),
+                                reader.GetString(5)
                                     }
-
-                                    return question;
-                                }
-                            }
-                            else
-                            {
-                                // Handle case when the query did not return any rows
-                                ErrorText.Text = "No rows returned by the query.";
+                                };
+                                allQuestions.Add(question);
                             }
                         }
+                    }
+
+                    // Shuffle questions
+                    Random rnd = new Random();
+                    allQuestions = allQuestions.OrderBy(x => rnd.Next()).ToList();
+
+                    // Ensure there are questions available
+                    if (allQuestions.Count > 0)
+                    {
+                        // Select the first question (after shuffling)
+                        Question selectedQuestion = allQuestions.First();
+
+                        // Remove displayed question ID
+                        displayedQuestionIds.Add(selectedQuestion.Id);
+                        if (displayedQuestionIds.Count == totalNumberOfQuestions)
+                        {
+                            displayedQuestionIds.Clear();
+                        }
+
+                        return selectedQuestion;
+                    }
+                    else
+                    {
+                        ErrorText.Text = "No questions available.";
                     }
                 }
             }
             catch (Exception ex)
             {
-                // Handle any exceptions (e.g., database connection error)
                 ErrorText.Text = "An error occurred: " + ex.Message;
             }
 
-            return null; // Handle case when no new question is found
+            return null;
         }
 
         // Method to get the current question
@@ -149,14 +256,33 @@ namespace UWP_Kviz
         {
             // Get the selected answer
             string selectedAnswer = GetSelectedAnswer();
-
             // Check if an answer is selected
             if (selectedAnswer != null)
             {
-                // Check if the selected answer is correct
-                if (selectedAnswer == currentQuestion.CorrectAnswer)
+                ErrorText.Text = selectedAnswer + Environment.NewLine;
+                ErrorText.Text += currentQuestion.CorrectAnswer;
+                if (selectedAnswer.Equals(currentQuestion.CorrectAnswer, StringComparison.OrdinalIgnoreCase))
                 {
-                    // If the answer is correct, generate a new question
+                    if (isFirstPlayerTurn)
+                        firstPlayerScore++;
+                    else
+                        secondPlayerScore++;
+
+                    // Update score text blocks
+                    PrviIgracBodovi.Text = firstPlayerScore.ToString();
+                    DrugiIgracBodovi.Text = secondPlayerScore.ToString();
+                    if (isFirstPlayerTurn)
+                        StopPlayer1Timer();
+                    else
+                        StopPlayer2Timer();
+
+                    isFirstPlayerTurn = !isFirstPlayerTurn;
+                    if (isFirstPlayerTurn)
+                        StartPlayer1Timer();
+                    else
+                        StartPlayer2Timer();
+                    PogresniOdgovori.Text = "";
+                    // Display new question
                     DisplayNewQuestion();
                 }
                 else
@@ -200,20 +326,25 @@ namespace UWP_Kviz
         // Method to display a new question
         private void DisplayNewQuestion()
         {
-            // Clear the error message
             ErrorText.Text = "";
+            PogresniOdgovori.Text = "";
+            // Clear the selection of radio buttons
+            optionRadioButton1.IsChecked = false;
+            optionRadioButton2.IsChecked = false;
+            optionRadioButton3.IsChecked = false;
+            optionRadioButton4.IsChecked = false;
 
             // Fetch and display a new question
-            Question randomQuestion = GetRandomQuestion();
-            if (randomQuestion != null)
+            currentQuestion = GetRandomQuestion(); // Update currentQuestion to the new question
+            if (currentQuestion != null)
             {
                 // Display the new question
-                questionTextBlock.Text = randomQuestion.QuestionText;
+                questionTextBlock.Text = currentQuestion.QuestionText;
 
                 // Shuffle the options
                 List<string> options = new List<string>();
-                options.Add(randomQuestion.CorrectAnswer);
-                options.AddRange(randomQuestion.WrongAnswers);
+                options.Add(currentQuestion.CorrectAnswer);
+                options.AddRange(currentQuestion.WrongAnswers);
                 Random rnd = new Random();
                 options = options.OrderBy(x => rnd.Next()).ToList();
 
